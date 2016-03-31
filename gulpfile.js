688194e86b08005d06741837872249b33670369a
Samuel <s-bridge@live.co.uk>
2016-03-31 12:21:23 +0100

    [Moved] - Moved everything around

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,62 @@
 var gulp = require('gulp'),
-    floeGulp = require('./floe-gulp/gulpfile.js');
+    gutil = require('gulp-util'),
+    webpack = require("webpack");
 
-gulp.task('default', () => {
-    
-    floeGulp.init(gulp);
-    
+gulp.task('default', ['webpack']);
+
+gulp.task('webpack', function() {
+
+    webpack({
+
+        watch: true,
+        entry: `${process.env.PWD}/js/src/zRS.js`,
+        output: {
+
+            path: `${process.env.PWD}/js/`,
+            filename: 'zRS.min.js'
+
+        },
+        module: {
+
+            loaders: [
+
+                {
+
+                    loader: 'babel-loader',
+                    query: {
+
+                        presets: ['es2015']
+
+                    },
+                    exclude: /(node_modules|bower_components)/
+
+                }
+
+            ]
+
+        },
+        plugins: [
+            new webpack.NoErrorsPlugin(),
+            new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+            new webpack.optimize.UglifyJsPlugin(),
+            new webpack.DefinePlugin({
+                'process.env': {
+
+                    'NODE_ENV': JSON.stringify('production'),
+                }
+            })
+        ]
+
+    }, function(err, stats) {
+
+        if(err) throw new gutil.PluginError("webpack", err);
+
+        gutil.log("[webpack]", stats.toString({
+            // output options
+        }));
+
+        gutil.log(gutil.colors.green('Webpack Compile Complete!'));
+
+    });
+
 });
