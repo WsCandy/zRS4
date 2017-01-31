a7a04d8eb5218afae276e8025789af6262aa9060
Samuel Woodbridge <s-bridge@live.co.uk>
2017-01-31 16:46:24 +0000

    [zRS] - Gulp removed, replaced with webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+
+	watch: true,
+	entry: `./src/zRS.js`,
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'zRS.min.js'
+	},
+	module: {
+		loaders: [
+			{
+				loader: 'babel-loader',
+				query: {
+					presets: ['es2015']
+				},
+				exclude: /(node_modules|bower_components)/
+			}
+		]
+	},
+	plugins: [
+		new webpack.optimize.UglifyJsPlugin()
+	]
+};
